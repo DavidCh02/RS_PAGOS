02d31b549610bc6239d511e63ff5975bb88e210f
Cristopher David Chingo Rivera <166523123+DavidCh02@users.noreply.github.com>
2025-11-24 23:36:03 -0500

    Update database connection details in database.pybase datos

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pytz
 
 # Configuración de la base de datos
-DATABASE_URL = "postgresql+pg8000://rs_pagos_user:XnBxCpRwG7C4Cb6jKIzZ2Wta3NJoOdfI@dpg-cvrlids9c44c73d6113g-a.oregon-postgres.render.com/rs_pagos"
+DATABASE_URL = "postgresql://rstorneos_user:6GWHEnUrLsGv7APN0xr7LQ6clLhr3nwy@dpg-d4ii9ik9c44c73asqqcg-a/rstorneos"
 engine = create_engine(DATABASE_URL)
 
 # Función para obtener una conexión a la base de datos
@@ -12,4 +12,5 @@ def get_db_connection():
     return engine.connect()
 
 # Obtener la fecha actual en UTC
+
 today_utc = datetime.now(pytz.timezone('UTC')).strftime('%Y-%m-%d')
